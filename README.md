1719cb30ab27a5e778af8f3884229fa40a0ee403
constDEFE <constant_defe@pm.me>
2023-01-29 22:08:43 +0600

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Markdown Previewer
+
+## _Vite, React, JS and SCSS_
+
+Random Quote Machine project from the FreeCodeCamp course.
---
+++ src/App.tsx
@@ -0,0 +1,64 @@
+import { marked } from "marked";
+import { ChangeEvent, FC, useState } from "react"
+import Prism from 'prismjs';
+import "./App.scss";
+
+marked.setOptions({
+  breaks: true,
+  highlight: (code, lang) => {
+    if (Prism.languages[lang]) {
+      return Prism.highlight(code, Prism.languages[lang], lang);
+    } else {
+      return code;
+    }
+  },
+});
+
+const renderer = new marked.Renderer();
+renderer.link = (href, title, text) => {
+  return `<a target="_blank" href="${href}">${text}</a>`;
+};
+
+interface EditorProps {
+  value: string;
+  onChange: (event: ChangeEvent<HTMLTextAreaElement>) => void;
+}
+
+const Editor: FC<EditorProps> = ({ value, onChange }) => {
+  return <textarea onChange={onChange} value={value} id="editor" />;
+};
+
+interface PreviewerProps {
+  content: string
+}
+
+const Previewer: FC<PreviewerProps> = ({ content }) => {
+  return (
+    <div id="preview" dangerouslySetInnerHTML={{
+      __html: marked(content, { renderer })
+    }}/>
+  );
+};
+
+const App: FC = () => {
+  const [text, setText] = useState<string>("dawd");
+
+  const handleChange = (
+    event: ChangeEvent<HTMLTextAreaElement>
+  ): void => {
+    setText(event.target.value);
+  }
+
+  return (
+    <main className="App">
+      <section>
+        <Editor value={text} onChange={handleChange} />
+      </section>
+      <section>
+        <Previewer content={text} />
+      </section>
+    </main>
+  );
+};
+
+export default App;
